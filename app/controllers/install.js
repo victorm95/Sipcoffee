dae4a44dfae864f23905f544b7197a54081ccda0
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2015-03-20 22:22:38 -0500

    Se continua con el modulo de instalacion.

--- app/controllers/install.js
+++ app/controllers/install.js
@@ -3,6 +3,15 @@ var routes = express.Router();
 
 routes.get('/install', function(req, res) {
 	res.render('install/index');
-});
+})
+
+.post('/install/land', function(req, res) {
+	var land = req.body;
+	land.blocks = parseInt(land.blocks);
+	land.area = parseInt(land.area);
+	res.json(land);
+})
+
+;
 
 module.exports = routes;
--- conf/db.js
+++ conf/db.js
@@ -4,7 +4,7 @@ module.exports = {
 	land: new Datastore({filename: 'db/land.db', autoload: true}), // Terreno
 	blocks: new Datastore({filename: 'db/blocks.db', autoload: true}), // Bloques
 	plots: new Datastore({filename: 'db/plots.db', autoload: true}), // Parcelas
-	harvests: new Datastore({filename: 'db/hasrvests,db', autoload: true}), // Cosechas
+	harvests: new Datastore({filename: 'db/harvests.db', autoload: true}), // Cosechas
 	sowings: new Datastore({filename: 'db/sowings.db', autoload: true}), // Siembras
 	seeds: new Datastore({filename: 'db/seeds.db', autoload: true}) // Cafetos (Semillas)
 };
