b0e270e79a4cba8ebc592262a7f33fff884b0275
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2015-03-23 17:59:57 -0500

    Terminando el modulo de Instalacion.

--- app/controllers/install.js
+++ app/controllers/install.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var routes = express.Router();
+var db = require('../../conf/db');
 
 routes.get('/install', function(req, res) {
 	res.render('install/index');
@@ -18,26 +19,29 @@ routes.get('/install', function(req, res) {
 		var block = {
 			name: req.body.blockName[i],
 			area: parseFloat(req.body.blockArea[i]),
-			plots: []
 		};
 		land.blocks.push(block);
 	}
 
 	if(blocksArea <= land.area) {
-		for(i=0; i<parseInt(req.body.blocks); i++) {
-			var plotsNum = parseInt(req.body.blockPlots[i]);
-			var plotArea = parseFloat(req.body.blockArea[i]) / parseFloat(req.body.blockPlots[i]);
+		db.land.insert(land, function(error, data) {
+			for(i=0; i<parseInt(req.body.blocks); i++) {
+				var plotsNum = parseInt(req.body.blockPlots[i]);
+				var plotArea = parseFloat(req.body.blockArea[i]) / parseFloat(req.body.blockPlots[i]);
 
-			for(j=1; j<=plotsNum; j++) {
-				var plot = {
-					name: 'Parcela '+j,
-					area: plotArea
-				};
-				land.blocks[i].plots.push(plot);
+				var plots = [];
+				for(j=1; j<=plotsNum; j++) {
+					var plot = {
+						name: 'Parcela '+j,
+						area: plotArea,
+						block_id: i
+					};
+					plots.push(plot);
+				}
+				db.plots.insert(plots, function(error, data) { });
 			}
-		}
-
-		res.json(land);
+			res.redirect('/');
+		});
 	} else {
 		res.send('Error, el area de los bloques exede el area el terreno.');
 	}
--- conf/db.js
+++ conf/db.js
@@ -2,7 +2,7 @@ var Datastore = require('nedb');
 
 module.exports = {
 	land: new Datastore({filename: 'db/land.db', autoload: true}), // Terreno
-	blocks: new Datastore({filename: 'db/blocks.db', autoload: true}), // Bloques
+	//blocks: new Datastore({filename: 'db/blocks.db', autoload: true}), // Bloques
 	plots: new Datastore({filename: 'db/plots.db', autoload: true}), // Parcelas
 	harvests: new Datastore({filename: 'db/harvests.db', autoload: true}), // Cosechas
 	sowings: new Datastore({filename: 'db/sowings.db', autoload: true}), // Siembras
