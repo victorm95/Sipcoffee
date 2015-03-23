a7c2381b1553fc29635235df2e79cda5f688e0a6
Dawverson Mejia <dawverson@gmail.com>
2015-03-23 17:41:47 -0500

    actualizacion dbs
    se modifica las dbs y el squema

--- conf/db.js
+++ conf/db.js
@@ -7,8 +7,8 @@ module.exports = {
 	harvests: new Datastore({filename: 'db/harvests.db', autoload: true}), // Cosechas
 	sowings: new Datastore({filename: 'db/sowings.db', autoload: true}), // Siembras
 	seeds: new Datastore({filename: 'db/seeds.db', autoload: true}), // Cafetos (Semillas)
-	processes: new Datastore({filename: 'db/processes', autoload: true}), // Procesos
-	entries: new Datastore({filename: 'db/entries', autoload: true}), // Entradas
-	outputs: new Datastore({filename: 'db/outputs', autoload: true}), // Salidas
-	products: new Datastore({filename: 'db/products', autoload: true}) // Productos	
+	io: new Datastore({filename: 'db/io', autoload: true}), // Entradasn y Salidas
+	inventory: new Datastore({filename: 'db/inventory', autoload: true}), // Inventario	
+	employees: new Datastore({filename: 'db/employees', autoload: true}), // Empleados
+	jobs: new Datastore({filename: 'db/jobs', autoload: true}) // trabajos
 };
