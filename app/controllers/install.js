d07773545be31fe7da3234c2686d39d3f01a19e4
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2015-03-22 22:26:55 -0500

    Se continua con el modulo de Instalacion.

--- app/controllers/install.js
+++ app/controllers/install.js
@@ -5,11 +5,42 @@ routes.get('/install', function(req, res) {
 	res.render('install/index');
 })
 
-.post('/install/land', function(req, res) {
-	var land = req.body;
-	land.blocks = parseInt(land.blocks);
-	land.area = parseInt(land.area);
-	res.json(land);
+.post('/install', function(req, res) {
+	var blocksArea = 0;
+	var land = {
+		name: req.body.name,
+		area: parseFloat(req.body.area),
+		blocks: []
+	};
+
+	for(i=0; i<parseInt(req.body.blocks); i++) {
+		blocksArea += parseFloat(req.body.blockArea[i]);
+		var block = {
+			name: req.body.blockName[i],
+			area: parseFloat(req.body.blockArea[i]),
+			plots: []
+		};
+		land.blocks.push(block);
+	}
+
+	if(blocksArea <= land.area) {
+		for(i=0; i<parseInt(req.body.blocks); i++) {
+			var plotsNum = parseInt(req.body.blockPlots[i]);
+			var plotArea = parseFloat(req.body.blockArea[i]) / parseFloat(req.body.blockPlots[i]);
+
+			for(j=1; j<=plotsNum; j++) {
+				var plot = {
+					name: 'Parcela '+j,
+					area: plotArea
+				};
+				land.blocks[i].plots.push(plot);
+			}
+		}
+
+		res.json(land);
+	} else {
+		res.send('Error, el area de los bloques exede el area el terreno.');
+	}
 })
 
 ;
---
+++ public/scripts/install.js
@@ -0,0 +1,78 @@
+// Elements
+var form = document.querySelector('form');
+var blocks =  form.querySelector('#blocks');
+
+// Events
+form.onreset = function(e) {
+	clearBlockForms();
+};
+
+blocks.oninput = function(e) {
+	clearBlockForms();
+	var numBlocks = parseInt(blocks.value);
+	for(i=1; i<=numBlocks; i++) {
+		var divForm = document.createElement('div');
+		divForm.className = 'form-block';
+
+		var divName = document.createElement('div');
+		divName.className = 'form-group';
+
+		var divArea = document.createElement('div');
+		divArea.className = 'form-group';
+
+		var divPlots = document.createElement('div');
+		divPlots.className = 'form-group';
+
+		var nameLabel = document.createElement('label');
+		nameLabel.setAttribute('for', 'blockName'+i);
+		nameLabel.appendChild(document.createTextNode('Nombre del bloque ' + i));
+
+		var nameInput = document.createElement('input');
+		nameInput.type = 'text';
+		nameInput.className = 'form-control';
+		nameInput.id = 'blockName'+i;
+		nameInput.name = 'blockName';
+		nameInput.placeholder = nameLabel.textContent;
+		nameInput.required = true;
+
+		var areaLabel = document.createElement('label');
+		areaLabel.setAttribute('for', 'blockArea'+i);
+		areaLabel.appendChild(document.createTextNode('Area del bloque ' + i));
+
+		var areaInput = document.createElement('input');
+		areaInput.type = 'number';
+		areaInput.className = 'form-control';
+		areaInput.id = 'blockArea'+i;
+		areaInput.name = 'blockArea';
+		areaInput.placeholder = areaLabel.textContent;
+		areaInput.required = true;
+
+		var plotsLabel = document.createElement('label');
+		plotsLabel.setAttribute('for', 'blockArea'+i);
+		plotsLabel.appendChild(document.createTextNode('Cantidad de parcelas en bloque ' + i));
+
+		var plotsInput = document.createElement('input');
+		plotsInput.type = 'number';
+		plotsInput.className = 'form-control';
+		plotsInput.id = 'blockPlots'+i;
+		plotsInput.name = 'blockPlots';
+		plotsInput.placeholder = plotsLabel.textContent;
+		plotsInput.required = true;
+
+		divName.appendChild(nameLabel);
+		divName.appendChild(nameInput);
+		divArea.appendChild(areaLabel);
+		divArea.appendChild(areaInput);
+		divPlots.appendChild(plotsLabel);
+		divPlots.appendChild(plotsInput);
+		divForm.appendChild(divName);
+		divForm.appendChild(divArea);
+		divForm.appendChild(divPlots);
+		form.insertBefore(divForm, form.lastChild);
+	}
+};
+
+function clearBlockForms() {
+	var blockForms = form.querySelectorAll('.form-block');
+	for(i=0; i<blockForms.length; i++) blockForms[i].remove();
+}
