9db204f6dd08d833975c81a02f5ef6527aaf9812
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2015-04-09 16:24:12 -0500

    Nuevo Bloque Se agrega la opcion para crear nuevos bloques.

--- app/controllers/blocks.js
+++ app/controllers/blocks.js
@@ -2,7 +2,36 @@ var express = require('express');
 var routes = express.Router();
 var db = require('../../conf/db');
 
-routes.get('/blocks/:id', function(req, res) {
+routes.get('/blocks/new', function(req, res) {
+	res.render('blocks/new');
+})
+.post('/blocks/new', function(req, res) {
+	db.land.findOne({}, function(error, land) {
+		var blocksArea = parseFloat(req.body.area);
+		for(i=0; i<land.blocks.length; i++) blocksArea += land.blocks[i].area;
+
+		if(blocksArea <= land.area) {
+			var block = {
+				name: req.body.name,
+				area: parseFloat(req.body.area),
+				_id: land.blocks[(land.blocks.length-1)]._id + 1
+			};
+			var plotArea = block.area / parseFloat(req.body.plots);
+			var plots = [];
+			for(i=0; i<parseFloat(req.body.plots); i++) plots.push({ name: 'Parcela '+(i<9?'0':'')+(i+1), area: plotArea, block_id: block._id});
+
+			db.land.update({_id: land._id}, {$push: {blocks: block}}, {}, function(error, num){
+				db.plots.insert(plots);			
+				res.redirect('/land');
+			});
+		} else {
+			res.render('error', {error: 'El area de los bloques es mayor al area del terreno.', page: req.originalUrl});
+		}
+	});
+})
+
+.get('/blocks/:id', function(req, res) {
+
 	db.land.findOne({}, function(error, land) {
 		db.plots.find({block_id: parseInt(req.params.id)}).sort({name: 1}).exec(function(error, data) {
 			var block = land.blocks[parseInt(req.params.id)];
