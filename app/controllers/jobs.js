86f260dfb841fcaf8b9e114fdf63f5e15c5d4e47
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2015-05-05 14:51:52 -0500

    Trabajos Se une el formulario generico para los trabajos con la tabla para la seleccion de las parcelas.

--- app/controllers/jobs.js
+++ app/controllers/jobs.js
@@ -2,25 +2,35 @@ var express = require('express');
 var routes = express.Router();
 var db = require('../../conf/db');
 
-routes.get('/plots/:plot_id/job/new/:type', function(req, res) {
+routes.get('/job/new/:type', function(req, res) {
 	var data = {};
 	db.employees.find({}, function(error, employees) {
 		data.employees = employees;
 		db.seeds.find({}, function(error, seeds) {
 			data.seeds = seeds;
-			res.render('jobs/new', {type: req.params.type, data: data});
+			db.land.findOne({}, function(error, land) {
+				db.plots.find({}, function(error, plots) {
+					var blocks = land.blocks;
+					for(i=0; i<plots.length; i++) {
+						if(!blocks[plots[i].block_id].plots) blocks[plots[i].block_id].plots = [];
+						blocks[plots[i].block_id].plots.push(plots[i]);
+					}
+					data.blocks = blocks;
+					res.render('jobs/new', {type: req.params.type, data: data});
+				});
+			});
 		});
 	})
 })
 
-.post('/plots/:plot_id/job/new/:type', function(req, res) {
+.post('/job/new/:type', function(req, res) {
 	var data = req.body;
 	data.type = req.params.type;
-	data.plot_id = req.params.plot_id;
 
-	db.jobs.insert(data, function(error, data) {
+	/*db.jobs.insert(data, function(error, data) {
 		res.redirect('/plots/'+req.params.plot_id);
-	});
+	});*/
+	res.json(data);
 })
 
 ;
